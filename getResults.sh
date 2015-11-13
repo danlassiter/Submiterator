464dddad9a33de27252d7c1a95c1b1c3d20d27f3
Dan Lassiter <danlassiter@stanford.edu>
2015-11-12 16:18:26 -0800

    Delete getResults.sh

--- getResults.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-pushd /Users/dan/aws-mturk-clt-1.3.1/bin
-./getResults.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 -successfile /Users/dan/Dropbox/Experiments/Submiterator/tagalog.success -outputfile /Users/dan/Dropbox/Experiments/Submiterator/tagalog.results
-popd
